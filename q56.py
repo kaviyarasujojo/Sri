b22777ea4e77ccff70f03ee8181c0f3e75d59dbb
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-02-08 21:56:59 +0530

    Update q56.py

--- q56.py
+++ q56.py
@@ -1,19 +1,32 @@
 a=input()
-b=True
+b=False
+c=False
+d=[]
 for i in a:
-	try:
-		if(int(i)>=0 or int(i)<=0):
-			b=True
-		else:
-			b=False
-			break
-	except ValueError:
-		if(i>='a' or i<='z'):
-			b=True
-		else:
-			b=False
-			break
-if(b==True):
-	print("Yes")
+    try:
+        if int(i)>=0 or int(i)<=0:
+            d.append(i)
+            continue
+    except ValueError:
+        f=True
+    if(65<=ord(i)<=90 or 97<=ord(i)<=122):
+        d.append(i)
+        continue
+for i in d:
+     try:
+        if int(i)>=0 or int(i)<=0:
+            b=True
+            continue
+        else:
+            b=False
+     except ValueError:
+         f=True
+     if(65<=ord(i)<=90 or 97<=ord(i)<=122):
+         c=True
+         continue
+     else:
+         c=False
+if (b==True and c==True):
+    print("Yes")
 else:
-	print("No")
+    print("No")
