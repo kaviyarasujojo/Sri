2948e9d72cc4bbafc6a852be12b7875c3088d192
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-06 15:29:05 +0530

    Update print_index.py

--- print_index.py
+++ print_index.py
@@ -1,10 +1,4 @@
 a=int(input())
-b=1
-c=[]
-while (b<=a):
-	d=int(input())
-	c.append(d)
-	b=b+1
-for i in range(0,b-1):
-	print(c[i],i)
-	
+b=input().split()
+for i in range(len(b)):
+	print(b[i],i)
