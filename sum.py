df325f22b04be45aa34db0d97971efb0a766b712
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-06 12:35:09 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,14 +1,13 @@
-n=int(input())
-m=int(input())
+n,m=input().split()
 a=0
 b=0
-while a<n:
+while a<int(n):
     a=a+1
     b=b+a
     print(a)
 a=0
 b=0
-while a<m:
+while a<int(m):
     a=a+1
     b=b+a
 print(b)
