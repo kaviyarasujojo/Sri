83aaafc5090a7cac60200dfddc7c202bd4033087
Srikumar-R <42167151+Srikumar-R@users.noreply.github.com>
2019-06-06 14:36:12 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,10 +1,8 @@
-n=int(input())
-a=int(input())
-d=int(input())
+n,a,d=input().split()
 c=1
 b=0
-while(c<=n):
-   b=b+a
-   a=a+d
+while(c<=int(n)):
+   b=b+int(a)
+   a=int(a)+int(d)
    c=c+1
 print(b)
